f9d85a24459ae4084f86068f28b4381c858e82f8
Ariel.Nackash <ariel.nackash@centrica.com>
2022-03-20 15:25:39 +0200

    implemented the Junit4 test calss

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -8,5 +8,7 @@ public class JavaHelloWorldTest {
 
     @Test
     public void getHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assertEquals("Hello World", javaHelloWorld.getHello());
     }
 }
